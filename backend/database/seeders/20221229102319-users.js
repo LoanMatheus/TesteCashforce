73eb2108bbaad7ea861bcfc6b6c13f31744e5d7e
LoanMatheus <loan.matheus@hotmail.com>
2022-12-29 08:07:08 -0300

    build seeders

---
+++ backend/database/seeders/20221229102319-users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [{
+      name: 'ALLAN',
+      email: 'allan@cashforce.com.br',
+      phoneNumber: null,
+      mobile: null,
+      departament: null,
+      verificationCode: '',
+      emailChecked: '1',
+      createdAt: '2020-10-01 21:31:37',
+      updatedAt: '2020-10-01 22:41:23',
+      cashforceAdm: '1'
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
